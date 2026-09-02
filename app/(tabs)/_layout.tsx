accbbab39f65b42b94a47b8a698d4fec1ea94571
kylayu <laurelyu58@gmail.com>
2026-09-02 23:12:44 +0800

    Initial commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,83 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#0088d1',
+        headerStyle: { backgroundColor: '#d6f4ff' },
+        headerShadowVisible: false,
+        headerTintColor: '#1e80a1',
+        tabBarStyle: { backgroundColor: '#d6f4ff' },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'home-sharp' : 'home-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="scan"
+        options={{
+          title: 'Scan',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'qr-code' : 'qr-code-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: 'History',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'time' : 'time-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'person' : 'person-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}        
+      />
+      <Tabs.Screen
+      name="teacher"
+      options={{
+        title: 'Teacher',
+        tabBarIcon: ({ color, focused }) => (
+        <Ionicons
+        name={focused ? 'clipboard' : 'clipboard-outline'}
+        color={color}
+        size={24}
+      />
+    ),
+  }}
+/>
+
+    </Tabs>
+  );
+}
---
+++ constants/colors.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: '#1565C0',
+  background: '#e4f9fe',
+  card: '#FFFFFF',
+  textPrimary: '#0D1B2A',
+  textSecondary: '#003f5c',
+  textOnPrimary: '#FFFFFF',
+  surface: '#E3F2FD',
+  border: '#E0E8F0',
+  shadow: '#0D47A1',
+} as const;
+
