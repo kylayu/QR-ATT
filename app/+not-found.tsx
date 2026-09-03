ea7a634fab754b24387d5bda3726ae6f17b4ae83
kylayu <laurelyu58@gmail.com>
2026-09-03 08:59:23 +0800

    module 3 and 4

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -15,11 +15,6 @@ export default function NotFoundScreen() {
 }
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
-  container: { flex: 1, backgroundColor: '#BFE7F5', justifyContent: 'center', alignItems: 'center' },
-  button: { fontSize: 20, textDecorationLine: 'underline', color: '#00141a' },
-=======
   container: { flex: 1, backgroundColor: '#25292e', justifyContent: 'center', alignItems: 'center' },
   button: { fontSize: 20, textDecorationLine: 'underline', color: '#fff' },
->>>>>>> 5c85b6e82ac2d3070d47078950be84ed94fa699d
 });
--- constants/colors.ts
+++ constants/colors.ts
@@ -1,17 +1,5 @@
 export const COLORS = {
   primary: '#1565C0',
-<<<<<<< HEAD
-  background: '#e4f9fe',
-  card: '#FFFFFF',
-  textPrimary: '#0D1B2A',
-  textSecondary: '#003f5c',
-  textOnPrimary: '#FFFFFF',
-  surface: 'rgb(244, 250, 255)',
-  border: '#E0E8F0',
-  shadow: '#0D47A1',
-} as const;
-
-=======
   background: '#F5F9FF',
   card: '#FFFFFF',
   textPrimary: '#0D1B2A',
@@ -21,4 +9,3 @@ export const COLORS = {
   border: '#E0E8F0',
   shadow: '#0D47A1',
 } as const;
->>>>>>> 5c85b6e82ac2d3070d47078950be84ed94fa699d
